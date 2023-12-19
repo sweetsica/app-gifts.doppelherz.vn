7fbb23974d8082c420584c4beb02f7ad5ae3cbca
Snow Deep <barondeepsnow@gmail.com>
2023-12-19 11:57:10 +0700

    change css

--- resources/views/customer/authenticated.blade.php
+++ resources/views/customer/authenticated.blade.php
@@ -8,10 +8,10 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
 
     <link rel="stylesheet" href="main.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css" />
     <style>
         * {
@@ -21,7 +21,7 @@
         }
 
         .main-content {
-            background-image: url('{{asset("assets/img/bg-img.jpg")}}');
+            background-image: url('{{ asset('assets/img/bg-img.jpg') }}');
             background-size: cover;
             background-repeat: no-repeat;
             width: 100%;
@@ -38,75 +38,102 @@
             border-radius: 50px;
             border: 1px solid #fff;
         }
+
+        .form-signup {
+            display: flex;
+            align-items: center;
+            justify-content: center
+        }
+
+        @media screen and (max-width: 480px) {
+            .form-signup {
+                display: block;
+            }
+
+            .main-button {
+                width: 100% !important;
+            }
+
+            .btn-signup {
+                width: 30% !important;
+            }
+
+            .form-control::placeholder {
+                font-size: 0.8rem;
+            }
+
+            .title {
+                font-size: 1.2rem;
+            }
+        }
     </style>
 </head>
 
 <body>
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
         integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-</script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-</script>
+    </script>
 
-<div class="main-content">
-    <div class="content text-center">
-        <div class="avatar mb-3" style="position: relative">
-            <img src="https://doppelherz.vn/wp-content/uploads/2022/12/LOGO-DOPPELHERZ-khong-khau-hieu-1000x800.png"
-                 class="img-thumbnail rounded mx-auto d-block"
-                 style="width: 80px; height: 80px; border-radius: 50% !important" alt="avatar" />
-
-        </div>
-        <H2 style="color: #ac1c1c">Xin chào {!! $datas['customerInfo']->name !!}</H2>
-{{--        <H3 style="color: #ac1c1c">Mã của bạn là: {!! $datas['customerInfo']->giftCode !!}</span></H3>--}}
-        <span class="text-white mt-3"><a href="https://doppelherz.vn"></a></span>
-        <div class="mt-3 text-center">
-            <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
-            <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
-            <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
-            <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
-        </div>
-        <div class="row mt-5">
-            <div class="col-12 mb-3">
-                <div class="d-flex align-items-center justify-content-center">
-                    <div class="main-button w-25 ">
-                        <a class="text-white" href='{!! $datas['customerInfo']->giftLink !!}'>SỬ DỤNG MÃ</a>
-                    </div>
-                </div>
+    <div class="main-content">
+        <div class="content text-center">
+            <div class="avatar mb-3" style="position: relative">
+                <img src="https://doppelherz.vn/wp-content/uploads/2022/12/LOGO-DOPPELHERZ-khong-khau-hieu-1000x800.png"
+                    class="img-thumbnail rounded mx-auto d-block" style="width: 80px; height: 80px" alt="avatar" />
 
             </div>
-            <div class="col-12 mb-3">
-                <div class="d-flex align-items-center justify-content-center">
-                    <div class="main-button w-25 ">
-                        <a class="text-white" href='https://doppelherz.vn'>Website</a>
+            <H2 style="color: #ac1c1c" class="title">Xin chào {!! $datas['customerInfo']->name !!}</H2>
+            {{--        <H3 style="color: #ac1c1c" class="title">Mã của bạn là: {!! $datas['customerInfo']->giftCode !!}</span></H3> --}}
+            <span class="text-white mt-3"><a href="https://doppelherz.vn"></a></span>
+            <div class="mt-3 text-center">
+                <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
+                <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
+                <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
+                <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
+            </div>
+            <div class="row mt-5">
+                <div class="col-12 mb-3">
+                    <div class="form-signup">
+                        <div class="main-button w-25 ">
+                            <a class="text-white" href='{!! $datas['customerInfo']->giftLink !!}'>SỬ DỤNG MÃ</a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-12 mb-3">
+                    <div class="form-signup">
+                        <div class="main-button w-25 ">
+                            <a class="text-white" href='https://doppelherz.vn'>Website</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-12 mb-3">
-                <div class="d-flex align-items-center justify-content-center">
-                    <div class="main-button w-25 ">
-                        <a class="text-white" href="tel:18001770">Hotline</a>
+                <div class="col-12 mb-3">
+                    <div class="form-signup">
+                        <div class="main-button w-25 ">
+                            <a class="text-white" href="tel:18001770">Hotline</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-12 mb-3">
-                <div class="d-flex align-items-center justify-content-center">
-                    <div class="main-button w-25 ">
-                        <a class="text-white" href='mailto:info@mastertran.vn'>Email</a>
+                <div class="col-12 mb-3">
+                    <div class="form-signup">
+                        <div class="main-button w-25 ">
+                            <a class="text-white" href='mailto:info@mastertran.vn'>Email</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-12 mb-3">
-                <a class="text-black" href='{{ route('logout') }}'>Đăng xuất</a>
+                <div class="col-12 mb-3">
+                    <a class="text-black" href='{{ route('logout') }}'>Đăng xuất</a>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </body>
 
 </html>
--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -8,10 +8,10 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
 
     <link rel="stylesheet" href="main.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css" />
     <style>
         * {
@@ -21,7 +21,8 @@
         }
 
         .main-content {
-            background-image: url('{{asset("assets/img/bg-img.jpg")}}');
+            background-image: url('{{ asset('assets/img/bg-img.jpg') }}');
+            /*background-image: url('./img/img-bg.jpg');*/
             background-size: cover;
             background-repeat: no-repeat;
             width: 100%;
@@ -38,89 +39,118 @@
             border-radius: 50px;
             border: 1px solid #fff;
         }
+
+        .form-signup {
+            display: flex;
+            align-items: center;
+            justify-content: center
+        }
+
+
+
+        @media screen and (max-width: 480px) {
+            .form-signup {
+                display: block;
+            }
+
+            .main-button {
+                width: 100% !important;
+            }
+
+            .content-signup {
+                width: 100% !important;
+            }
+
+            .btn-signup {
+                width: 30% !important;
+                top: 8% !important;
+                padding: 13px !important;
+            }
+
+            .form-control::placeholder {
+                font-size: 0.8rem;
+            }
+        }
     </style>
 </head>
 
 <body>
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
         integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-</script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-</script>
+    </script>
 
-<div class="main-content">
-    <div class="content text-center">
-        <div class="avatar mb-3" style="position: relative">
-            <img src="https://doppelherz.vn/wp-content/uploads/2022/12/LOGO-DOPPELHERZ-khong-khau-hieu-1000x800.png"
-                 class="img-thumbnail rounded mx-auto d-block"
-                 style="width: 80px; height: 80px; border-radius: 50% !important" alt="avatar" />
-            <i class="bi bi-check-circle-fill text-primary"
-               style="position: absolute; top: 0; right: 48%; z-index: 99"></i>
-        </div>
-        <span class="text-white mt-3"><a href="https://doppelherz.vn">@doppelherz</a></span>
-        <div class="mt-3 text-center">
-            <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
-            <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
-            <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
-            <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
-        </div>
-
-        <div class="mt-5">
-            <form action="{{ route('check.code') }}" method="POST"
-                  class="d-flex align-items-center justify-content-center">
-                @csrf
-                <div class="input-group mb-3 w-25" style="position: relative">
-                    <input type="text" class="form-control" placeholder="Vui lòng nhập mã khuyến mãi"
-                           aria-label="Vui lòng nhập mã khuyến mãi" aria-describedby="button-addon2"
-                           style="border-radius: 50px; padding: 30px" name="code">
-
-                    <input type="hidden" value="2023" name="token">
-                    <div class="input-group-append">
-                        <button type="submit" id="button-addon2" class="btn btn-danger fw-bold"
-                                style="background-color: #ac1c1c;
-                                position: absolute;
-                                width: 25%;
-                                border-radius: 50px;
-                                padding: 12px;
-                                right: 1%;
-                                top: 10%;
-                                z-index: 99;">Truy cập</button>
-{{--                        <input type="submit" value="Truy cập">--}}
-                    </div>
-                </div>
-            </form>
-        </div>
-        <div class="row">
-            <div class="col-12 mb-3">
-                <div class="d-flex align-items-center justify-content-center">
-                    <div class="main-button w-25 ">
-                        <a class="text-white" href='https://doppelherz.vn'>Website</a>
-                    </div>
-                </div>
+    <div class="main-content">
+        <div class="content text-center">
+            <div class="avatar mb-3">
+                <img src="https://doppelherz.vn/wp-content/uploads/2022/12/LOGO-DOPPELHERZ-khong-khau-hieu-1000x800.png"
+                    class="img-thumbnail rounded mx-auto d-block" style="width: 80px; height: 80px" alt="avatar" />
             </div>
-            <div class="col-12 mb-3">
-                <div class="d-flex align-items-center justify-content-center">
-                    <div class="main-button w-25 ">
-                        <a class="text-white" href="tel:18001770">Hotline</a>
+            <span class="text-white mt-3"><a href="https://doppelherz.vn">@doppelherz</a></span>
+            <div class="mt-3 text-center">
+                <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
+                <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
+                <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
+                <span><i class="bi bi-instagram mx-2" style="color: #ac1c1c"></i></span>
+            </div>
+
+            <div class="mt-5">
+                <form action="{{ route('check.code') }}" method="POST" class="form-signup">
+                    @csrf
+                    <div class="input-group mb-3 w-25 content-signup" style="position: relative">
+                        <input type="text" class="form-control" placeholder="Vui lòng nhập mã khuyến mãi"
+                            aria-label="Vui lòng nhập mã khuyến mãi" aria-describedby="button-addon2"
+                            style="border-radius: 50px; padding: 30px" name="code">
+
+                        <input type="hidden" value="2023" name="token">
+                        <div class="input-group-append">
+                            <button type="submit" id="button-addon2" class="btn btn-danger fw-bold btn-signup"
+                                style=" background-color: #ac1c1c;
+                            position: absolute;
+                            width: 25%;
+                            border-radius: 50px;
+                            padding: 12px;
+                            right: 1%;
+                            top: 10%;
+                            z-index: 99;">Truy
+                                cập</button>
+                                 {{--                        <input type="submit" value="Truy cập"> --}}
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="row">
+                <div class="col-12 mb-3">
+                    <div class="form-signup">
+                        <div class="main-button w-25">
+                            <a class="text-white" href='https://doppelherz.vn'>Website</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-12 mb-3">
-                <div class="d-flex align-items-center justify-content-center">
-                    <div class="main-button w-25 ">
-                        <a class="text-white" href='mailto:info@mastertran.vn'>Email</a>
+                <div class="col-12 mb-3">
+                    <div class="form-signup">
+                        <div class="main-button w-25">
+                            <a class="text-white" href="tel:18001770">Hotline</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 mb-3">
+                    <div class="form-signup">
+                        <div class="main-button w-25">
+                            <a class="text-white" href='mailto:info@mastertran.vn'>Email</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </body>
 
 </html>
